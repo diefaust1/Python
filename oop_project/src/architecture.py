8cae0be37413b919b22372d37c18a98f3ae80223
Eric <ericfaust@web.de>
2026-01-20 21:50:58 +0100

    changed oop project

--- oop_project/src/architecture.py
+++ oop_project/src/architecture.py
@@ -1,10 +1,28 @@
+import subprocess
+import os
+
 class Commands:
 
     HELP = "-help"
     EXIT = "-exit"
+    CLEAR = "-clear"
+    GOLD = "-gold"
     
     DESCRIPTION = {
         HELP:"shows all available commands",
-        EXIT: "exits the game"
+        EXIT: "exits the game",
+        CLEAR: "clears the terminal from prior output",
+        GOLD: "shows the players gold"
     }
+
+    @classmethod
+    def help_command(cls):
+        for key, value in cls.DESCRIPTION.items():
+            print(f"{key}: {value}")
     
+    def clear_terminal(self):
+        command = "cls" if os.name == "nt" else "clear"
+        subprocess.run(command, shell=True)
+
+    def show_gold(gold):
+        print(str(gold) + " gold")
--- oop_project/src/main.py
+++ oop_project/src/main.py
@@ -1,23 +1,37 @@
-import architecture as arch
+import architecture as arch, time
+
 
 print("Welcome to Adventure Time...")
 print("To see the possible commmands type -help")
 
 command = arch.Commands()
+MINIMAL_GOLD = 5
+gold = 0
 
-def help_command():
-    for key, value in command.DESCRIPTION.items():
-        print(key, value)
+start_time = time.monotonic()
 
-while(True):
+while True:
     
+    current_time = time.monotonic()
+    time_passed = current_time - start_time
+    gold = gold + time_passed
+    start_time = current_time
+
     user_input = input()
     match user_input:
-        case command.HELP:
-            help_command()
         
-        case "-exit":
+        case command.HELP:
+            command.help_command()
+        
+        case command.EXIT:
             break
+        
+        case command.CLEAR:
+            command.clear_terminal()
+        
+        case "-gold":
+            print(round(gold, 3))
+        
         case _:
             print("command not found")
 
